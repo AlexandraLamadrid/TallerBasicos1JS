26dc5a00c30637f722fa781c12c8a0af594663c8
Daniel Zamora Paez <[email>
2025-07-11 12:23:36 -0500

    Subida ejercicio 3, arreglo posibles fallos en desarrollo

--- Ejercicios3.js
+++ Ejercicios3.js
@@ -5,3 +5,20 @@ const inventario = [
     {nombre:"Chaqueta Chevignon", precio:200000},
     {nombre:"Chaqueta Luis Vouton", precio:500000}
 ]
+
+function buscarProducto(){
+    let nombreBuscar = String(prompt("Ingrese el nombre del producto a buscar: "));
+    let d = false;
+    for(i=0; inventario.length; i++){
+        if (nombreBuscar == inventario[i].nombre){
+            console.log("Producto encontrado: ", inventario[i].nombre, " con precio: ", inventario[i].precio);
+            d = true
+            break;
+        }        
+    }
+    if(d !== true){
+        console.log("PRODUCTO NO ENCONTRADO EN INVENTARIO");
+    }
+}
+
+buscarProducto();
