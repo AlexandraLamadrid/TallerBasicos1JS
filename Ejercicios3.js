6178f1f98502dda1d794620f01af835794291446
Daniel Zamora Paez <[email>
2025-07-11 12:02:31 -0500

    Ajuste organizacion ejercicios

---
+++ Ejercicios3.js
@@ -0,0 +1,7 @@
+/*EJERCICIO 3*/
+const prompt = require("prompt-sync")();
+
+const inventario = [
+    {nombre:"Chaqueta Chevignon", precio:200000},
+    {nombre:"Chaqueta Luis Vouton", precio:500000}
+]
